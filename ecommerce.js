7fabe321f934fd6ca1dbc6e32b543fc24d0be544
ginona <ginonacchio94@gmail.com>
2020-08-21 20:25:34 -0300

    Agrego al carrito

--- ecommerce.js
+++ ecommerce.js
@@ -94,18 +94,11 @@ let productos = [
 // function comprarCarrito () {
 
 // }
-// {
-//     id: 4,
-//     nombre: 'Fresas',
-//     precio: 0.6,
-//     imagen: 'fresas.jpg'
-// }
+
 let $items = document.querySelector('#items');
 let carrito1 = [];
 let total = 0;
 let $carrito = document.querySelector('#carrito');
-let $total = document.querySelector('#total');
-let $botonVaciar = document.querySelector('#boton-vaciar');
 
 function renderItems(){
     for(let info of productos){
@@ -132,7 +125,7 @@ function renderItems(){
                miNodoBoton.classList.add('btn', 'btn-secondary');
                miNodoBoton.textContent = 'Agregar al carrito';
                miNodoBoton.setAttribute('id', info['id']);
-               miNodoBoton.addEventListener('click', aniadirCarrito);
+               miNodoBoton.addEventListener('click', llenarCarrito);
                // Insertamos
                miNodoCardBody.appendChild(miNodoImagen);
                miNodoCardBody.appendChild(miNodoTitle);
@@ -143,96 +136,23 @@ function renderItems(){
     }
 }
 
-    function aniadirCarrito(){
-        carrito1.push(this.getAttribute('id'));
-        // let itemLista = document.createElement('li');
-        // itemLista.textContent = prod.descripcion;
-        // calcularTotal();
-        console.log(carrito1);
-        // $carrito.appendChild(itemLista);
-        
+    function llenarCarrito(){
+        let valorId = this.getAttribute('id');
+        let objetoSeleccionado = productos.filter(item => item.id == valorId);
+        carrito1.push(objetoSeleccionado);
+        renderizarCarrito()
     }
     
+     function renderizarCarrito() {
+    console.log(carrito1);
 
-    // function renderizarCarrito() {
-    //     // Vaciamos todo el html
-    //     $carrito.textContent = '';
-    //     // Quitamos los duplicados
-    //     let carritoSinDuplicados = [...new Set(carrito)];
-    //     // Generamos los Nodos a partir de carrito
-    //     carritoSinDuplicados.forEach(function (item, indice) {
-    //         // Obtenemos el item que necesitamos de la variable base de datos
-    //         let miItem = baseDeDatos.filter(function(itemBaseDatos) {
-    //             return itemBaseDatos['id'] == item;
-    //         });
-    //         // Cuenta el número de veces que se repite el producto
-    //         let numeroUnidadesItem = carrito.reduce(function (total, itemId) {
-    //             return itemId === item ? total += 1 : total;
-    //         }, 0);
-    //         // Creamos el nodo del item del carrito
-    //         let miNodo = document.createElement('li');
-    //         miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-    //         miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0]['nombre']} - ${miItem[0]['precio']}€`;
-    //         // Boton de borrar
-    //         let miBoton = document.createElement('button');
-    //         miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-    //         miBoton.textContent = 'X';
-    //         miBoton.style.marginLeft = '1rem';
-    //         miBoton.setAttribute('item', item);
-    //         miBoton.addEventListener('click', borrarItemCarrito);
-    //         // Mezclamos nodos
-    //         miNodo.appendChild(miBoton);
-    //         $carrito.appendChild(miNodo);
-    //     })
-    // }
+            let miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            for(let i = 0; i < carrito1.length; i++){
+                miNodo.textContent = carrito1[i].descripcion;
 
-    // function calcularTotal() {
-    //     // Limpiamos precio anterior
-    //     total = 0;
-    //     // Recorremos el array del carrito
-    //     for (let item of carrito) {
-    //         // De cada elemento obtenemos su precio
-    //         let miItem = baseDeDatos.filter(function(itemBaseDatos) {
-    //             return itemBaseDatos['id'] == item;
-    //         });
-    //         total = total + miItem[0]['precio'];
-    //     }
-    //     // Formateamos el total para que solo tenga dos decimales
-    //     let totalDosDecimales = total.toFixed(2);
-    //     // Renderizamos el precio en el HTML
-    //     $total.textContent = totalDosDecimales;
-    // }
+            }
+     }
 
 renderItems();
-
-// class Productos {
-//     constructor(nombrePr, precioPr) {
-//         this.nombrePr = nombrePr;
-//         this.precioPr = precioPr;
-//     }
-// }
-
-// var listadoPr = [];
-
-// botonEnviarPr.onclick = (function(e) {
-//     nombrePr = document.querySelector('#nombre-pr');
-//     precioPr = document.querySelector('#precio-pr');
-//     let product = new Productos(nombrePr.value, precioPr.value);
-//     listadoPr.push(product);
-//     nombrePr.value = '';
-//     precioPr.value = '';
-//     e.preventDefault();
-// });
-
-// botonListadoPr.onclick = (function(e) {
-    
-//     for(let i = 0; i < listadoPr.length; i++) {
-//         let itemListado = `Nombre: ${listadoPr[i].nombrePr} Precio: ${listadoPr[i].precioPr}`
-//         let li = document.createElement('li');
-//         li.innerText = itemListado;
-//         lista.appendChild(li);
-//     }
-//     e.preventDefault();
-//     return false;
-// });
 }
